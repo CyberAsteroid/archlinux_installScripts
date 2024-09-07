967f22f9b7eec8244f1a8448a3ac751b9fbb3863
CyberAsteroid <1071608290@qq.com>
2024-09-07 16:17:35 +0800

    Update arch_graphics.sh

--- arch_graphics.sh
+++ arch_graphics.sh
@@ -5,9 +5,9 @@
 # 如果你想安装gnome桌面，添加gnome gnome-extra
 
 
-sudo pacman -S vulkan-intel lib32-vulkan-intel mesa lib32-mesa alsa alsa-utils pipewire pipewire-pulse pavucontrol xorg xorg-xinit sddm fcitx5-git fcitx5-qt5 fcitx5-qt6 fcitx5-gtk fcitx5-configtool fcitx5-chinese-addons fcitx5-pinyin-moegirl fcitx5-pinyin-zhwiki yay git feh rofi flameshot v2ray v2raya telegram-desktop vlc obs-studio mpv netease-cloud-music icalingua ttf-dejavu wqy-microhei wqy-zenhei
+sudo pacman -S vulkan-intel lib32-vulkan-intel mesa lib32-mesa pipewire pipewire-pulse pavucontrol xorg xorg-xinit sddm fcitx5 fcitx5-gtk fcitx5-configtool fcitx5-chinese-addons fcitx5-pinyin-moegirl fcitx5-pinyin-zhwiki yay git feh rofi flameshot v2ray v2raya telegram-desktop vlc obs-studio mpv ttf-dejavu wqy-microhei wqy-zenhei
 
 
-echo "GTK_IM_MODULE DEFAULT=fcitx" >> ~/.pam_environment
-echo "QT_IM_MODULE  DEFAULT=fcitx" >> ~/.pam_environment
-echo "XMODIFIERS    DEFAULT=\@im=fcitx" >> ~/.pam_environment
+echo "GTK_IM_MODULE DEFAULT=fcitx" >> /etc/security/pam_env.conf
+echo "QT_IM_MODULE  DEFAULT=fcitx" >> /etc/security/pam_env.conf
+echo "XMODIFIERS    DEFAULT=\@im=fcitx" >> /etc/security/pam_env.conf
